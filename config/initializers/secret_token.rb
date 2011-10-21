c6343c455eff284ff48f16bafa6841455191fe17
root <root@AstraL.(none)>
2011-10-21 10:54:27 +0300

    qweqwe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linx::Application.config.secret_token = 'cd1b231182e71d336b86d42d798f7534e1b0b48a4a19222ed27e220770a1db42247bdc38aea17a8a17c065ded7de348e3c4bed182ba0ee765d4273d8ec0a4888'
